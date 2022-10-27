b01d62c9a5572eb6803f0e0883ebd0b43a57ee57
Zoli <suveges.zoltan@petrik.hu>
2022-10-27 09:49:11 +0200

    asd

--- src/main/java/hu/petrik/helloword/HelloController.java
+++ src/main/java/hu/petrik/helloword/HelloController.java
@@ -1,14 +1,28 @@
 package hu.petrik.helloword;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label koszontoSzoveg;
+    @FXML
+    private TextField nevMezo;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void koszontes(ActionEvent actionEvent) {
+            String nev = nevMezo.getText().trim();
+
+        if (nev.isEmpty()) {
+            Alert felugroAblak = new Alert(Alert.AlertType.WARNING);
+            felugroAblak.setContentText("Kérem adja meg a nevét!");
+            felugroAblak.showAndWait()  ;
+            koszontoSzoveg.setText("");
+        } else {
+            koszontoSzoveg.setText("Hello " + nev + "!");
+        }
     }
 }
